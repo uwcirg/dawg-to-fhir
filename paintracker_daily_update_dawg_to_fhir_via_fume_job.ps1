9cf91c9f3125e61aefb02b4069e7b1cbeacb556b
James Sibley <jsibley@uw.edu>
2024-07-03 16:37:17 -0700

    Update paintracker_daily_update_dawg_to_fhir_via_fume_job.ps1
    Fix wacky caps!

--- paintracker_daily_update_dawg_to_fhir_via_fume_job.ps1
+++ paintracker_daily_update_dawg_to_fhir_via_fume_job.ps1
@@ -4,7 +4,7 @@ Add-content $Logfile -value "[$Timestamp] Starting up FUME API..."
 Start-Job { cd C:\Users\jsibley\Documents\fume-community; npm start }
 Start-Sleep -Seconds 30
 $Timestamp = Get-Date -UFormat "%Y-%m-%d %T"
-Add-content $Logfile -value "[$Timestamp] Running PaInTRaCkEr daIlY UpDaTe daWg-tO-FhIr script..."
+Add-content $Logfile -value "[$Timestamp] Running PainTracker Daily Update DAWG-to-FHIR script..."
 cd "C:\Users\jsibley\Documents"
 Invoke-Expression "& python 'C:\Users\jsibley\Documents\paintracker_daily_update_dawg_to_fhir_via_fume.py' "
 $Timestamp = Get-Date -UFormat "%Y-%m-%d %T"
